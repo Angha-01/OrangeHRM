b3fa0e4e1583a1ec814f0a0fb3a6d696b441191b
Angha-01 <anghaambulge2@gmail.com>
2024-06-10 19:51:26 +0530

    To Run valid & invalid testcases

--- src/main/java/com/baseclass/BaseClass.java
+++ src/main/java/com/baseclass/BaseClass.java
@@ -1,5 +1,7 @@
 package com.baseclass;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,7 +12,10 @@ public class BaseClass {
 	public static WebDriver getWebDriver() {
 	    driver=new ChromeDriver();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.manage().window().maximize();
+		
 		return driver;
 	}
 }
+
--- src/test/java/com/tests/FindBy.java
+++
@@ -1,5 +0,0 @@
-package com.tests;
-
-public @interface FindBy {
-
-}
