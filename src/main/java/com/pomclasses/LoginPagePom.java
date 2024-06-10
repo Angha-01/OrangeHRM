0b2cb8b82788d091cbb78fb8b8828495bbb58b1e
Angha-01 <anghaambulge2@gmail.com>
2024-06-10 19:34:21 +0530

    LoginPagePom: "To Run invalid testcase & valid testcase"

--- src/main/java/com/pomclasses/LoginPagePom.java
+++ src/main/java/com/pomclasses/LoginPagePom.java
@@ -9,19 +9,19 @@ import com.baseclass.BaseClass;
 public class LoginPagePom extends BaseClass {
 	
 	public LoginPagePom() {
-		PageFactory.initElements(getWebDriver(),this);
+		PageFactory.initElements(driver,this); 		//getWebDriver()
 	}
-	@FindBy(xpath="//p[text()='Username:Admin']")
+	@FindBy(xpath="//p[text()='Username : Admin']")
 	private WebElement usernameText;
 	
-	@FindBy(xpath="//p[text()='Password:admin123']")
+	@FindBy(xpath="//p[text()='Password : admin123']")
 	private WebElement passwordText;
 	
 	@FindBy(xpath="//input[@name='username']")
-	private WebElement username;
+	private WebElement usernameinput;
 	
 	@FindBy(xpath="//input[@name='password']")
-	private WebElement password;
+	private WebElement passwordinput;
 	
 	@FindBy(xpath="//button[@type='submit']")
 	private WebElement submitbutton;
@@ -29,4 +29,30 @@ public class LoginPagePom extends BaseClass {
 	@FindBy(xpath="//div[@class='orangehrm-login-forgot']//child::p")
 	private WebElement forgotpassword;
 	
+	@FindBy(xpath = "//p[text()='Invalid credentials']")
+	private WebElement Invalid_credentials;
+	
+	public String getusername() {
+		String username=usernameText.getText();
+		System.out.println(username);
+		return username.substring(username.indexOf('A'));
+	}
+	public String getpassword() {
+		String password=passwordText.getText();
+		System.out.println(password);
+		return password.substring(password.lastIndexOf('a'));
+	}
+	public String getinvalidmsg() {
+		String invalidmsg=Invalid_credentials.getText();
+		return invalidmsg;
+	}
+	
+	public void login(String username,String password) throws InterruptedException {
+		usernameinput.sendKeys(username);
+		passwordinput.sendKeys(password);
+		System.out.println("Enter the username &password");
+		submitbutton.click();
+		Thread.sleep(5000);
+		
+	}
 }
