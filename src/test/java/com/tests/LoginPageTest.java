8eac719b8b0a877dee98d7571664c8b669da986b
Angha-01 <anghaambulge2@gmail.com>
2024-06-10 19:44:13 +0530

    LoginPageTest:"Run Valid & Invalid Testcases"

--- src/test/java/com/tests/LoginPageTest.java
+++ src/test/java/com/tests/LoginPageTest.java
@@ -5,7 +5,9 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.baseclass.BaseClass;
+import com.pomclasses.LoginPagePom;
 public class LoginPageTest extends BaseClass {
+	
 	@BeforeClass
 	public void setup() {
 		getWebDriver();
@@ -18,12 +20,25 @@ public class LoginPageTest extends BaseClass {
 	@Test
 	public void pageUrl() {
 		
-		String pageurl=getWebDriver().getCurrentUrl();
+		String pageurl=driver.getCurrentUrl();			//getWebDriver()
 		if(pageurl.equalsIgnoreCase("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"));
 		{
 			System.out.println("We are in correct page: Test pass");
 		}
 		
 	}
-	 
+	@Test
+	public void loginTest() throws InterruptedException {
+		LoginPagePom loginpagepom=new LoginPagePom();
+		loginpagepom.login(loginpagepom.getusername(),loginpagepom.getpassword());
+		System.out.println("Login sucessful");
+	}
+	@Test
+	public void loginInvalidTest() throws InterruptedException {
+		LoginPagePom loginpagepom=new LoginPagePom();
+		loginpagepom.login(loginpagepom.getusername(),"123admin");
+		System.out.println(loginpagepom.getinvalidmsg());
+		
+	}
+	
 }
